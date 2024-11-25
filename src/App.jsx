ff616002863879c694ba0c0814e3642244a69018
OomkarPatil <omkarpatil5053@gmail.com>
2024-11-25 15:13:41 +0530

    fully complete website

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 import Hero from './components/hero'
 import About from './components/About'
 import NavBar from './components/NavBar'
+import Features from './components/Features'
+import Story from './components/Story'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
-      <NavBar/>
+      <NavBar />
       <Hero />
-      <About/>
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
     </main>
 
   )
